d12132ea7b2b9dbcbc8757562437660188456cc3
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-31 18:48:18 +0100

    Code refactored with classes

--- script.js
+++ script.js
@@ -3,32 +3,37 @@ const string =
 
 const object = JSON.parse(string);
 
-console.log(object);
+class Image {
+  constructor(url, description) {
+    this.url = url;
+    this.description = description;
+  }
+  render() {
+    const div = document.createElement("div");
+    div.innerHTML = `<div class="image"><img src="${this.url}" alt=${this.description}></div><div class="image__description">${this.description}</div>`;
+    return div;
+  }
+}
 
 const gallery = document.querySelector(".gallery");
 
-console.log(gallery);
+// const getHtmlForImage = (img) => {
+//   const url = img["url"];
+//   const description = img["description"];
+//   return '<img src="' + url + '" alt="' + description + '">';
+// };
 
-const getHtmlForImage = (img) => {
-  const url = img["url"];
-  const description = img["description"];
-  return '<img src="' + url + '" alt="' + description + '">';
-};
-
-const getHtmlForDescription = (des) => {
-  const description = des["description"];
-  return '<div class="image__description">' + description + "</div>";
-};
+// const getHtmlForDescription = (des) => {
+//   const description = des["description"];
+//   return '<div class="image__description">' + description + "</div>";
+// };
 
 object.forEach((element) => {
-  //   console.log(element);
-  //   const url = element["url"];
-  //   const description = element["description"];
-  //   console.log(description);
-
-  gallery.innerHTML +=
-    '<div class="image">' +
-    getHtmlForImage(element) +
-    getHtmlForDescription(element) +
-    "</div>";
+  const image = new Image(element.url, element.description);
+  gallery.appendChild(image.render());
+  // gallery.innerHTML +=
+  //   '<div class="image">' +
+  //   getHtmlForImage(element) +
+  //   getHtmlForDescription(element) +
+  //   "</div>";
 });
